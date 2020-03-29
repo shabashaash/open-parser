0ecb8c1b8e0a41aef652ef50a6c6a9a469d3f7a7
shabashaash <43162766+shabashaash@users.noreply.github.com>
2020-03-29 22:56:43 +0300

    Update settings.py

--- ai-parser/settings.py
+++ ai-parser/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'parser_api_app',
+    'ai-parser.parser_api_app',
     'channels',
 ]
 
