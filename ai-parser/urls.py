c0bfb0feb5de1c861b67c32ceee4fc96f4b40bbf
shabashaash <43162766+shabashaash@users.noreply.github.com>
2020-03-29 22:49:57 +0300

    Update urls.py

--- ai-parser/urls.py
+++ ai-parser/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('parser_api_app.urls')),
+    path('api/', include('urls')),
 ]
