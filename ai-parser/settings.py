c861539a64f569a4e0a02674fb7d61e5033736c6
shabashaash <43162766+shabashaash@users.noreply.github.com>
2020-03-30 00:48:50 +0300

    Update settings.py

--- ai-parser/settings.py
+++ ai-parser/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
-DEBUG = False
+DEBUG = os.getenv("DJANGO_SECRET_KEY",True)
 
 REST_FRAMEWORK = {       
     'DEFAULT_RENDERER_CLASSES': (       
