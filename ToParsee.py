7c515cc9e37b230162d09b20585f3e15a96a3ef5
shabashaash <43162766+shabashaash@users.noreply.github.com>
2020-03-30 19:07:09 +0300

    Update ToParsee.py

--- ToParsee.py
+++ ToParsee.py
@@ -837,12 +837,12 @@ async def generateP(url,models,url_):
     # print(columns)
     arr = []
     elems = []
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless') 
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('headless') 
     d = DesiredCapabilities.CHROME
     # d['loggingPrefs'] = {'browser':'ALL'}
     #driver = webdriver.PhantomJS()#.Chrome("/opt/app-root/src/chromedriver.exe",options=options)
-    driver = webdriver.Remote(command_executor='http://selenium-openshift-ai-parser.apps.us-east-1.starter.openshift-online.com/wd/hub',desired_capabilities=d,options=options)
+    driver = webdriver.Remote(command_executor='http://selenium-openshift-ai-parser.apps.us-east-1.starter.openshift-online.com:4444/wd/hub',desired_capabilities=d)#,options=options)
     driver.get(url) #https://rbb-holod.ru/catalog/freony-xladony https://morena.ru/catalog/teploobmennoe-oborudovanie/ http://www.aholod.ru/catalog/42/ https://www.eldorado.ru/c/stiralnye-mashiny/
     js = """
                 var res = [];
